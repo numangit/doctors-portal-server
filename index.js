17a74ea54c4777a550890430be2386c47092b374
numangit <abdulnumanjob@gmail.com>
2022-11-16 14:52:00 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+//middle ware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    res.send('Doctors portal server is running')
+});
+
+app.listen(port, () => {
+    console.log(`Doctors portal running on ${port}`)
+});
