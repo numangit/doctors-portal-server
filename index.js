08efa96e4916e4fcabf5d14359aed9ba3c6a7002
numangit <abdulnumanjob@gmail.com>
2022-11-16 15:06:50 +0600

    mongodb connection, DBuser created and environment variable added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -7,6 +9,11 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(express.json());
+
+//connection with mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zbie1as.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 app.get('/', async (req, res) => {
     res.send('Doctors portal server is running')
