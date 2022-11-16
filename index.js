843676df11a70cd688949ceb0f2d1d1cce8eab99
numangit <abdulnumanjob@gmail.com>
2022-11-16 19:40:42 +0600

    show the remaining slots functionality added in appointmentOptions get api

--- index.js
+++ index.js
@@ -20,21 +20,21 @@ async function run() {
         const bookingsCollection = client.db('doctorsPortal').collection('bookings');
 
         app.get('/appointmentOptions', async (req, res) => {
-            // const date = req.query.date;
+            const date = req.query.date;
             const query = {};
             const options = await appointmentOptionCollection.find(query).toArray();
 
             // get the bookings of the provided date
-            // const bookingQuery = { appointmentDate: date }
-            // const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray();
+            const bookingQuery = { appointmentDate: date }
+            const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray();
 
             // code carefully :D
-            // options.forEach(option => {
-            //     const optionBooked = alreadyBooked.filter(book => book.treatment === option.name);
-            //     const bookedSlots = optionBooked.map(book => book.slot);
-            //     const remainingSlots = option.slots.filter(slot => !bookedSlots.includes(slot))
-            //     option.slots = remainingSlots;
-            // })
+            options.forEach(option => {
+                const optionBooked = alreadyBooked.filter(book => book.treatment === option.name);
+                const bookedSlots = optionBooked.map(book => book.slot);
+                const remainingSlots = option.slots.filter(slot => !bookedSlots.includes(slot))
+                option.slots = remainingSlots;
+            })
             res.send(options);
         });
         app.post('/bookings', async (req, res) => {
