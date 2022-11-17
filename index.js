90bc4194c617a53c1c7e45e8d0dcb27742ff990a
numangit <abdulnumanjob@gmail.com>
2022-11-17 22:06:14 +0600

    api to grant a user admin role is created

--- index.js
+++ index.js
@@ -126,6 +126,28 @@ async function run() {
             res.send(result);
         });
 
+        //api to grant a user admin role
+        app.put('/users/admin/:id', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const query = { email: decodedEmail };
+            const user = await usersCollection.findOne(query);
+
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
+
     }
     finally {
 
