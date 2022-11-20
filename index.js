89c661cc423e2e213a568a7a31dfef3d5791d0db
numangit <abdulnumanjob@gmail.com>
2022-11-20 18:31:31 +0600

    payment intents api for strip and api to store payment details added

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId, ObjectID } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const port = process.env.PORT || 5000;
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
+const port = process.env.PORT || 5000;
 const app = express();
 
 //middleware
@@ -40,6 +41,7 @@ async function run() {
         const bookingsCollection = client.db('doctorsPortal').collection('bookings');
         const usersCollection = client.db('doctorsPortal').collection('users');
         const doctorsCollection = client.db('doctorsPortal').collection('doctors');
+        const paymentsCollection = client.db('doctorsPortal').collection('payments');
 
         //middleware to verify if the user is admin (NOTE: make sure you use verifyAdmin after verifyJWT so we have double layer security and you can use the decoded email, so only user with token can reach this level)
         const verifyAdmin = async (req, res, next) => {
@@ -124,6 +126,43 @@ async function run() {
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
         });
+
+        /*
+        ----------------- PAYMENT ----------------------
+        */
+        //Payment intents API to get client secret from strip
+        app.post('/create-payment-intent', async (req, res) => {
+            const booking = req.body;
+            const price = booking.price;
+            const amount = price * 100;
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: 'usd',
+                amount: amount,
+                "payment_method_types": [
+                    "card"
+                ]
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
+
+        //api to store the payment information in payment collection
+        app.post('/payments', async (req, res) => {
+            const payment = req.body;
+            const result = await paymentsCollection.insertOne(payment);
+            const id = payment.bookingId
+            const filter = { _id: ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    paid: true,
+                    transactionId: payment.transactionId
+                }
+            }
+            const updatedResult = await bookingsCollection.updateOne(filter, updatedDoc)
+            res.send(result);
+        })
 
         //endpoint to generate jwt (client side will send query)
         app.get('/jwt', async (req, res) => {
